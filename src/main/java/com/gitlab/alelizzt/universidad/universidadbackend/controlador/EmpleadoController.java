145a44f7dc56f64bec38821f8c0498721ee9f417
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-07 17:16:47 -0500

    Encapsulando el Response del controlador Empleado

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/EmpleadoController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/EmpleadoController.java
@@ -8,9 +8,12 @@ import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.Em
 import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.PersonaDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -34,23 +37,35 @@ public class EmpleadoController extends PersonaController{
     }*/
 
     @GetMapping("/tipoempleado")
-    public List<Persona> buscarEmpleadoPorTipoEmpleado(@RequestParam String tipoEmpleado){
+    public ResponseEntity<?> buscarEmpleadoPorTipoEmpleado(@RequestParam String tipoEmpleado){
+        Map<String, Object> mensaje = new HashMap<>();
         List<Persona> empleadoByTipoEmpleado = (List<Persona>) ((EmpleadoDAO)service).findEmpleadoByTipoEmpleado(tipoEmpleado);
         if(empleadoByTipoEmpleado.isEmpty()){
-            throw new BadRequestException(String.format("No se ha encontrado algun empleado de tipo %s", tipoEmpleado));
+            //throw new BadRequestException(String.format("No se ha encontrado algun empleado de tipo %s", tipoEmpleado));
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No se ha encontrado algun empleado de tipo %s", tipoEmpleado));
+            return ResponseEntity.badRequest().body(mensaje);
         }
-        return empleadoByTipoEmpleado;
+        mensaje.put("datos", empleadoByTipoEmpleado);
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
     }
 
     @PutMapping("{idEmpleado}/pabellon")
-    public Persona asignarPabellonEmpleado(@RequestParam Integer idEmpleado, @RequestBody Pabellon pabellon){
+    public ResponseEntity<?> asignarPabellonEmpleado(@RequestParam Integer idEmpleado, @RequestBody Pabellon pabellon){
+        Map<String, Object> mensaje = new HashMap<>();
         Optional<Persona> oEmpleado = service.findById(idEmpleado);
         if(!oEmpleado.isPresent()){
-            throw new BadRequestException(String.format("El/La empleado/a con id %d no existe", idEmpleado));
+            //throw new BadRequestException(String.format("El/La empleado/a con id %d no existe", idEmpleado));
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("El/La empleado/a con id %d no existe", idEmpleado));
+            return ResponseEntity.badRequest().body(mensaje);
         }
         Empleado empleado = (Empleado)oEmpleado.get();
         empleado.setPabellon(pabellon);
 
-        return service.save(empleado);
+        mensaje.put("datos", service.save(empleado));
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
     }
 }
