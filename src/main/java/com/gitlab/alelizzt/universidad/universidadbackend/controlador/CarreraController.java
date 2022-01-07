8ba6c445ed294dd359147fb0285984ec112ffac9
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-06 19:55:24 -0500

    Agregando metodos expuestos del servicio al controlador de la entidad Carrera

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/CarreraController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/CarreraController.java
@@ -67,4 +67,50 @@ public class CarreraController extends GenericController<Carrera, CarreraDAO>{
         return ResponseEntity.ok(mensaje);
     }
 
+    @GetMapping("/carrera/contains/")
+    public ResponseEntity<?> buscarCarrerasQueContenganNombre(@RequestParam String nombre) {
+        Map<String, Object> mensaje = new HashMap<>();
+        List<Carrera> carrerasByNombreContains = (List<Carrera>) service.findCarrerasByNombreContains(nombre);
+        if (carrerasByNombreContains.isEmpty()) {
+            //throw new BadRequestException(String.format("No existen carreras con %s en su nombre", nombre));
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No existen carreras con %s en su nombre", nombre));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos", carrerasByNombreContains);
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
+    }
+
+
+    @GetMapping("/carrera/contains/ignore/")
+    public ResponseEntity<?> buscarCarrerasQueContenganNombreNoCase(@RequestParam String nombre) {
+        Map<String, Object> mensaje = new HashMap<>();
+        List<Carrera> carrerasByNombreContainsIgnoreCase = (List<Carrera>) service.findCarrerasByNombreContainsIgnoreCase(nombre);
+        if (carrerasByNombreContainsIgnoreCase.isEmpty()) {
+            //throw new BadRequestException(String.format("No existen carreras con %s en su nombre", nombre));
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No existen carreras con %s en su nombre", nombre));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos", carrerasByNombreContainsIgnoreCase);
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
+    }
+
+    @GetMapping("/carrera/anios/")
+    public ResponseEntity<?> buscarCarrerasConCantidadAnios(@RequestParam Integer cantAnios) {
+        Map<String, Object> mensaje = new HashMap<>();
+        List<Carrera> carrerasByCantidadAniosAfter= (List<Carrera>) service.findCarrerasByCantidadAniosAfter(cantAnios);
+        if (carrerasByCantidadAniosAfter.isEmpty()) {
+            //throw new BadRequestException(String.format("No existen carreras con %s en su nombre", nombre));
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No existen carreras con %d cantidad de años", cantAnios));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos", carrerasByCantidadAniosAfter);
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
+    }
+
 }
