48e5571dfe20bb9e52c9f98f75166f7c1b848201
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-08 19:18:47 -0500

    Generando el DTO de la entidad Aula

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/ProfesorController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/ProfesorController.java
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/profesores")
---
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/modelo/entidades/dto/AulaDTO.java
@@ -0,0 +1,27 @@
+package com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.dto;
+
+import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.enumeradores.Pizarron;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AulaDTO {
+    @NotNull
+    private Integer id;
+    @Positive(message = "El valor no puede ser negativo")
+    private Integer nroAula;
+    @NotEmpty(message = "Debe ingresar medidas en metros cuadrados")
+    private String medidas;
+    @Positive(message = "El valor no puede ser negativo")
+    private Integer cantidadPupitres;
+    @NotNull
+    @NotEmpty(message = "Debe ingresar un valor")
+    private Pizarron pizarron;
+}
