3bf402525efe07181f703c4a18a35781119793ef
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-11 16:45:49 -0500

    Personalizando la informacion de la API

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/configuration/SpringFoxSwagger.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/configuration/SpringFoxSwagger.java
@@ -1,10 +1,39 @@
 package com.gitlab.alelizzt.universidad.universidadbackend.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxSwagger {
-    
+
+    @Bean
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gitlab.alelizzt.universidad.universidadbackend.controlador.dto"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Universidad Backend API",
+                "API para el manejo de nuestra universidad",
+                "V2",
+                "Terminos del servicio",
+                new Contact("Alejandro Lizzt", "https://github.com/Alelizzt","fakemail@gmail.com"),
+                "Licencia de API", "url de licencia", Collections.emptyList()
+        );
+    }
 }
