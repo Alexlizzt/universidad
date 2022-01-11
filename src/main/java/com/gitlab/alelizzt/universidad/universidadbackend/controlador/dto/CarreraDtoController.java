e6e1e70c69549eed8c1628cc1bbf32bc6743305f
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-11 17:04:34 -0500

    Documentando el controlador dto de Carrera

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
@@ -5,21 +5,25 @@ import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.dto.C
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.mapper.CarreraMapper;
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.mapper.mapstruct.CarreraMapperMS;
 import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.CarreraDAO;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/carreras")
 @ConditionalOnProperty(prefix = "app", name = "controller.enable-dto", havingValue = "true")
+@Api(value = "Aplicaciones relacionadas con las carreras", tags = "Acciones sobre Carreras")
 public class CarreraDtoController extends GenericDtoController<Carrera, CarreraDAO>{
 
     @Autowired
@@ -30,6 +34,10 @@ public class CarreraDtoController extends GenericDtoController<Carrera, CarreraD
     }
 
     @GetMapping
+    @ApiOperation(value = "Consultar todas las carreras")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Ejecutado satisfactoriamente")
+    })
     public ResponseEntity<?> obtenerCarreras(){
         Map<String, Object> mensaje = new HashMap<>();
         List<Carrera> carreras = super.obtenerTodos();
@@ -47,6 +55,22 @@ public class CarreraDtoController extends GenericDtoController<Carrera, CarreraD
         mensaje.put("success", Boolean.TRUE);
         mensaje.put("data", carreraDTOS);
 
+        return ResponseEntity.ok(mensaje);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Consultar carrera por codigo")
+    public ResponseEntity<?> obtenerCarreraPorId(@PathVariable @ApiParam(name = "Codigo del sistema") Integer id){
+        Map<String, Object> mensaje = new HashMap<>();
+        Optional<Carrera> oCarrera = service.findById(id);
+        if(!oCarrera.isPresent()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("La carrera con id %d no existe", id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+
+        mensaje.put("datos", oCarrera.get());
+        mensaje.put("success", Boolean.TRUE);
         return ResponseEntity.ok(mensaje);
     }
 }
