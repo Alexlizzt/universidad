9b97e1113db5957906c81dcd0ee8c54c00b85d4c
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-11 22:06:22 -0500

    Arreglando codigos de estado y agregando metodos CRUD base para Carrera

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/CarreraDtoController.java
@@ -8,9 +8,12 @@ import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.Ca
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +27,7 @@ import java.util.stream.Collectors;
 public class CarreraDtoController extends GenericDtoController<Carrera, CarreraDAO>{
 
     @Autowired
-    private CarreraMapperMS mapper;
+    private CarreraMapperMS carreraMapper;
 
     public CarreraDtoController(CarreraDAO service) {
         super(service, "Carrera");
@@ -37,7 +40,7 @@ public class CarreraDtoController extends GenericDtoController<Carrera, CarreraD
     })
     public ResponseEntity<?> obtenerCarreras(){
         Map<String, Object> mensaje = new HashMap<>();
-        List<Carrera> carreras = super.obtenerTodos();
+        List<Carrera> carreras = (List<Carrera>) service.findAll();
 
         if(carreras.isEmpty()){
             mensaje.put("success", Boolean.FALSE);
@@ -45,18 +48,33 @@ public class CarreraDtoController extends GenericDtoController<Carrera, CarreraD
             return ResponseEntity.badRequest().body(mensaje);
         }
 
-        List<CarreraDTO> carreraDTOS = carreras
+        /*List<CarreraDTO> carreraDTOS = carreras
                 .stream()
-                .map(mapper::mapCarrera)
-                .collect(Collectors.toList());
+                .map(carreraMapper::mapCarrera)
+                .collect(Collectors.toList());*/
+        List<CarreraDTO> carreraDTOS = carreraMapper.mapCarreras(carreras);
+
         mensaje.put("success", Boolean.TRUE);
         mensaje.put("data", carreraDTOS);
-
         return ResponseEntity.ok(mensaje);
     }
 
+    @PostMapping
+    @ApiOperation(value = "Agregar Carrera")
+    public ResponseEntity<?> agregarCarrera(@Valid @RequestBody @ApiParam(name = "Carrera de la universidad") CarreraDTO carreraDTO, BindingResult result){
+        Map<String, Object> mensaje = new HashMap<>();
+        if(result.hasErrors()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("validaciones", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos", super.agregarEntidad(carreraMapper.mapCarrera(carreraDTO)));
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mensaje);
+    }
+
     @GetMapping("/{id}")
-    @ApiOperation(value = "Consultar carrera por codigo")
+    @ApiOperation(value = "Consultar Carrera por codigo")
     public ResponseEntity<?> obtenerCarreraPorId(@PathVariable @ApiParam(name = "Codigo del sistema") Integer id){
         Map<String, Object> mensaje = new HashMap<>();
         Optional<Carrera> oCarrera = service.findById(id);
@@ -66,16 +84,39 @@ public class CarreraDtoController extends GenericDtoController<Carrera, CarreraD
             return ResponseEntity.badRequest().body(mensaje);
         }
 
-        mensaje.put("datos", oCarrera.get());
+        mensaje.put("datos", carreraMapper.mapCarrera(oCarrera.get()));
         mensaje.put("success", Boolean.TRUE);
         return ResponseEntity.ok(mensaje);
     }
 
-    @PostMapping
-    public ResponseEntity<?> agregarCarrera(@RequestBody @ApiParam(name = "Carrera de la universidad") Carrera carrera){
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Actualizar los datos de la Carrera")
+    public ResponseEntity<?> actualizarCarrera(@PathVariable Integer id, @RequestBody CarreraDTO carreraDTO){
         Map<String, Object> mensaje = new HashMap<>();
-        mensaje.put("datos", service.save(carrera));
+        Carrera carreraUpdate = null;
+        Optional<Carrera> oCarrera = service.findById(id);
+        if(!oCarrera.isPresent()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("la carrera con id %d no existe", id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        Carrera carrera = carreraMapper.mapCarrera(carreraDTO);
+
+        carreraUpdate = oCarrera.get();
+        carreraUpdate.setNombre(carrera.getNombre());
+        carreraUpdate.setCantidadAnios(carrera.getCantidadAnios());
+        carreraUpdate.setCantidadMaterias(carrera.getCantidadMaterias());
+
+        mensaje.put("datos", service.save(carreraUpdate));
         mensaje.put("success", Boolean.TRUE);
-        return ResponseEntity.ok().body(mensaje);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(mensaje);
     }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Eliminar la carrera del sistema")
+    public ResponseEntity<?> borrarCarrera(@PathVariable Integer id){
+        service.deteteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
 }
