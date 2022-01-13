c6ad7bc0938892b62ad5a0061a141e80445916df
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-13 13:10:23 -0500

    Agregando CRUD al controlador dto de Profesor

--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/ProfesorDtoController.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/controlador/dto/ProfesorDtoController.java
@@ -3,32 +3,131 @@ package com.gitlab.alelizzt.universidad.universidadbackend.controlador.dto;
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.Carrera;
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.Persona;
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.Profesor;
+import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.dto.PersonaDTO;
+import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.dto.ProfesorDTO;
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.mapper.mapstruct.AlumnoMapper;
+import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.mapper.mapstruct.ProfesorMapper;
 import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.PersonaDAO;
 import com.gitlab.alelizzt.universidad.universidadbackend.servicios.contratos.ProfesorDAO;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/profesores")
 @ConditionalOnProperty(prefix = "app", name = "controller.enable-dto", havingValue = "true")
+@Api(value = "Aplicaciones relacionadas con los profesores", tags = "Acciones sobre Profesores")
 public class ProfesorDtoController extends PersonaDtoController {
 
-    private final ProfesorDAO profesorDAO;
 
-    public ProfesorDtoController(@Qualifier("profesorDAOImpl") PersonaDAO service, AlumnoMapper alumnoMapper, ProfesorDAO profesorDAO) {
-        super(service, "Profesor", alumnoMapper);
-        this.profesorDAO = profesorDAO;
+    public ProfesorDtoController(@Qualifier("profesorDAOImpl") PersonaDAO service, ProfesorMapper profesorMapper) {
+        super(service, "Profesor", profesorMapper);
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Consultar todos los profesores")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Ejecutado satisfactoriamente")
+    })
+    public ResponseEntity<?> obtenerProfesores(){
+        Map<String, Object> mensaje = new HashMap<>();
+        Stream<Persona> personas = ((List<Persona>) super.obtenerTodos()).stream();
+        List<Persona> profesores = personas.filter(persona -> persona instanceof Profesor).collect(Collectors.toList());
+
+        if (profesores.isEmpty()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No se encontrarn los %ses cargadas", nombre_entidad));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        List<ProfesorDTO> profesorDTOS = profesorMapper.mapProfesor(profesores);
+
+        mensaje.put("success", Boolean.TRUE);
+        mensaje.put("data", profesorDTOS);
+        return ResponseEntity.ok(mensaje);
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Agregar profesor")
+    public ResponseEntity<?> agregarProfesor(@Valid @RequestBody @ApiParam(name = "Profesor de la universidad")PersonaDTO personaDTO, BindingResult result) {
+        Map<String, Object> mensaje = new HashMap<>();
+        if(result.hasErrors()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("validaciones", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+
+        PersonaDTO save = super.agregarPersona(profesorMapper.mapProfesor((ProfesorDTO) personaDTO));
+        mensaje.put("success", Boolean.TRUE);
+        mensaje.put("data", save);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(mensaje);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Consultar profesor por codigo")
+    public ResponseEntity<?> obtenerProfesorPorId(@PathVariable @ApiParam(name = "Codigo del sistema") Integer id) {
+        Map<String, Object> mensaje = new HashMap<>();
+        PersonaDTO dto = super.buscarPersonaPorId(id);
+
+        if (dto == null){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No existe %s con ID %d", nombre_entidad, id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+
+        mensaje.put("success", Boolean.TRUE);
+        mensaje.put("data", profesorMapper.mapProfesor((ProfesorDTO) dto));
+
+        return ResponseEntity.ok(mensaje);
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Actualizar los datos del profesor")
+    public ResponseEntity<?> actualizarProfesor(@PathVariable Integer id, @RequestBody @ApiParam(name = "Profesor de la universidad") ProfesorDTO profesorDTO, BindingResult result) {
+        Map<String, Object> mensaje = new HashMap<>();
+        Profesor profesorUpdate = null;
+        PersonaDTO personaDTO = super.buscarPersonaPorId(id);
+        if(result.hasErrors()){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("validaciones", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+
+        Profesor profesor = profesorMapper.mapProfesor(profesorDTO);
+
+        profesorUpdate = profesorMapper.mapProfesor((ProfesorDTO) personaDTO);
+        profesorUpdate.setNombre(profesor.getNombre());
+        profesorUpdate.setApellido(profesor.getApellido());
+        profesorUpdate.setDni(profesor.getDni());
+        profesorUpdate.setDireccion(profesor.getDireccion());
+        profesorUpdate.setSueldo(profesor.getSueldo());
+
+        mensaje.put("datos", service.save(profesorUpdate));
+        mensaje.put("success", Boolean.TRUE);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(mensaje);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Eliminar profesor del sistema")
+    public ResponseEntity<?> borrarProfesor(@PathVariable Integer id){
+        service.deteteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
     @GetMapping("/carrera")
-    public ResponseEntity<?> buscarProfesoresPorCarrera(@RequestParam String carrera){
+    @ApiOperation(value = "Buscar Profesores en base a la carrera")
+    public ResponseEntity<?> buscarProfesoresPorCarrera(@RequestParam @ApiParam(name = "Carrera de la universidad")String carrera){
         Map<String, Object> mensaje = new HashMap<>();
-        List<Persona> profesoresByCarrera = (List<Persona>) profesorDAO.findProfesoresByCarrera(carrera);
+        List<Persona> profesoresByCarrera = (List<Persona>)((ProfesorDAO)service).findProfesoresByCarrera(carrera);
         if(profesoresByCarrera.isEmpty()){
             //throw new BadRequestException("No se ha encontrado algun profesor");
             mensaje.put("success", Boolean.FALSE);
@@ -40,8 +139,10 @@ public class ProfesorDtoController extends PersonaDtoController {
         return ResponseEntity.ok(mensaje);
     }
 
+    // TODO: Verificar funcionamiento
     @PutMapping("{idProfesor}/carrera/{idCarreras}")
-    public ResponseEntity<?> asignarCarrerasProfesor(@PathVariable Integer idProfesor, @PathVariable Set<Carrera> idCarreras){
+    @ApiOperation(value = "Asignar Carreras al profesor")
+    public ResponseEntity<?> asignarCarrerasProfesor(@PathVariable Integer idProfesor, @RequestBody Set<Carrera> idCarreras){
         Map<String, Object> mensaje = new HashMap<>();
         Optional<Persona> oProfesor = service.findById(idProfesor);
         if(!oProfesor.isPresent()){
--- src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/modelo/entidades/dto/ProfesorDTO.java
+++ src/main/java/com/gitlab/alelizzt/universidad/universidadbackend/modelo/entidades/dto/ProfesorDTO.java
@@ -1,17 +1,23 @@
 package com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.dto;
 
 import com.gitlab.alelizzt.universidad.universidadbackend.modelo.entidades.Direccion;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ApiModel(description = "Profesor de la universidad", value = "Profesor", reference = "Profesor")
 public class ProfesorDTO extends PersonaDTO{
 
+    @ApiModelProperty(name = "Sueldo del profesor" , example = "5000.00")
+    @NotEmpty
     private BigDecimal sueldo;
 
     public ProfesorDTO(Integer id, String nombre, String apellido, String dni, Direccion direccion, BigDecimal sueldo) {
